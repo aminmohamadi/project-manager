7f3730651a2b15fe4b102c2fe7ff17e848904d1f
aminm <amin.mohamadi13238@gmail.com>
2022-10-14 17:10:49 +0330

    1401/07/21

--- utils/server.js
+++ utils/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+}
 const app = express().listen(80,()=>{
     console.log('http://localhost')
 })
-
-
-
+ module.exports = app
